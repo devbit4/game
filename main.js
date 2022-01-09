f023f12603c250edcc4874c13a216958954203fd
devbit4 <devbit4@gmail.com>
2022-01-09 11:33:40 +0900

    edit game place

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-const space = document.querySelector(".game");
+const space = document.querySelector('.game-space');
 const cal = space.getBoundingClientRect();
-const start = document.querySelector(".start");
-const stop = document.querySelector(".stop");
-const time = document.querySelector(".time");
-const pop = document.querySelector(".pop-up");
-
+const start = document.querySelector('.start');
+const stop = document.querySelector('.stop');
+const time = document.querySelector('.time');
+const pop = document.querySelector('.pop-up');
+const replay = document.querySelector('.replay');
+const score = document.querySelector('.game-score');
+const fishes = document.querySelectorAll('.fish');
+const message = document.querySelector('.message');
 const lastTime = 5;
-
-
-console.log(time.innerHTML)
+let clock;
+let points = 5;
 
 // 게임준비
 function spacing() {
-
-    objects("shark", 5, "img/shark.png");
-    objects("fish", 5, "img/fish.png");
-    console.log(cal);
+    space.innerHTML = '';
+    objects('shark', 5, 'img/shark.png');
+    objects('fish', 5, 'img/fish.png');
 }
 
 function objects(name, number, src) {
@@ -23,11 +24,12 @@ function objects(name, number, src) {
     const height = cal.height - 80;
 
     for (let i = 0; i < number; i++) {
-        const object = document.createElement("img");
-        object.setAttribute("src", src);
-        object.style.position = "absolute";
+        const object = document.createElement('img');
+        object.classList.add(name);
+        object.setAttribute('src', src);
+        object.style.position = 'absolute';
         const x = random(0, width);
-        const y = random(0, height);
+        const y = random(20, height);
         object.style.left = `${x}px`;
         object.style.top = `${y}px`;
         space.appendChild(object);
@@ -40,35 +42,54 @@ function random(min, max) {
 
 // 게임시작
 
-start.addEventListener("click", () => {
-    start.style.display = "none";
+start.addEventListener('click', () => {
+    start.style.display = 'none';
+    stop.classList.remove('stop-hide');
+    gameStart();
+});
+
+function gameStart() {
     timer();
     spacing();
-    stop.classList.remove("stop-hide");
-
-})
+    score.innerHTML = points;
+}
 
 // 타이머
 
 function timer() {
     let last = lastTime;
-    let min = "";
-    let sec = "";
+    let min = '';
+    let sec = '';
 
-    let x = setInterval(function () {
+    clock = setInterval(function () {
         --last;
         min = parseInt(last / 60);
         sec = last % 60;
-        time.innerHTML = min + ":" + sec;
+        time.innerHTML = min + ':' + sec;
         if (last <= 0) {
-            clearInterval(x);
-            time.innerHTML = "OVER";
+            clearInterval(clock);
+            time.innerHTML = 'OVER';
+            gameStop();
         }
     }, 1000);
 }
 
 // 게임중지
 
-stop.addEventListener("click", () => {
-    pop.classList.remove("hide");
-})
+stop.addEventListener('click', () => {
+    gameStop();
+});
+function gameStop() {
+    pop.classList.remove('hide');
+    clearInterval(clock);
+    time.innerHTML = '0:0';
+    points = 5;
+}
+
+// 게임재시작
+replay.addEventListener('click', () => {
+    pop.classList.add('hide');
+    gameStart();
+});
+
+
