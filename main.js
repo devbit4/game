9d2f63dce9afb7220a7bb9d0358c1223fed98277
devbit4 <devbit4@gmail.com>
2022-01-22 01:36:24 +0900

    add game rule

--- main.js
+++ main.js
@@ -8,14 +8,70 @@ const replay = document.querySelector('.replay');
 const score = document.querySelector('.game-score');
 const fishes = document.querySelectorAll('.fish');
 const message = document.querySelector('.message');
-const lastTime = 5;
+const lastTime = 6;
 let clock;
 let points = 5;
+//character info
+const char1 = "fish";
+const char2 = "shark";
+const char1_num = 5;
+const char2_num = 5;
+const char1_imgPath = 'img/fish.png';
+const char2_imgPath = 'img/shark.png';
+const char1_width = 50;
+const char2_width = 80;
+
+const rule = document.querySelector(".rule");
+const ruleClose = document.querySelector(".close");
+const ruleOpen = document.querySelector(".ruleBtn");
+
+ruleClose.addEventListener("click", () => {
+    rule.style.display = "none";
+})
+ruleOpen.addEventListener("click", () => {
+    rule.style.display = "block";
+})
+
+// 게임시작
+
+start.addEventListener('click', () => {
+    start.style.display = 'none';
+    stop.classList.remove('stop-hide');
+    gameStart();
+});
+
+stop.addEventListener('click', () => {
+    gameStop();
+});
+
+// 게임재시작
+replay.addEventListener('click', () => {
+    pop.classList.add('hide');
+    gameStart();
+});
+
+// 점수내기
+space.addEventListener('click', (e) => {
+    if (e.target.className === char1) {
+        e.target.remove();
+        points--;
+        score.innerHTML = points;
+        if (points === 0) {
+            gameStop();
+            message.innerHTML = 'You Won!';
+        }
+    } else if (e.target.className == char2) {
+        e.target.remove();
+        gameStop();
+        message.innerHTML = 'You Lost!';
+    }
+});
+
 
 // 게임준비
 function spacing() {
-    objects('shark', 5, 'img/shark.png');
-    objects('fish', 5, 'img/fish.png');
+    objects(char1, char1_num, char1_imgPath);
+    objects(char2, char2_num, char2_imgPath);
 }
 
 function objects(name, number, src) {
@@ -39,13 +95,7 @@ function random(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-// 게임시작
 
-start.addEventListener('click', () => {
-    start.style.display = 'none';
-    stop.classList.remove('stop-hide');
-    gameStart();
-});
 
 function gameStart() {
     space.classList.add("active");
@@ -79,38 +129,14 @@ function timer() {
 
 // 게임중지
 
-stop.addEventListener('click', () => {
-    gameStop();
-});
 function gameStop() {
     space.innerHTML = '';
     space.classList.remove("active");
     pop.classList.remove('hide');
     clearInterval(clock);
     time.innerHTML = 'over';
-
     points = 5;
 }
 
-// 게임재시작
-replay.addEventListener('click', () => {
-    pop.classList.add('hide');
-    gameStart();
-});
+// game rule
 
-// 점수내기
-space.addEventListener('click', (e) => {
-    if (e.target.className === 'fish') {
-        e.target.remove();
-        points--;
-        score.innerHTML = points;
-        if (points === 0) {
-            gameStop();
-            message.innerHTML = 'You Won!';
-        }
-    } else if (e.target.className == 'shark') {
-        e.target.remove();
-        gameStop();
-        message.innerHTML = 'You Lost!';
-    }
-});
